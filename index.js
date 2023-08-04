f38db73e2434f3063dcb36d4133fe2ca10248e14
zesanahmed <zesanahmed593@gmail.com>
2023-08-04 11:18:43 +0600

    user selected class delete operation added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware 
@@ -42,15 +42,32 @@ async function run() {
         })
 
 
-        // cart collection
-        app.post('/carts', async (req, res) => {
-            const classCart = req.body;
-            console.log(classCart);
-            const result = await cartCollection.find().toArray();
+        // cart collection apis
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
 
 
+        app.post('/carts', async (req, res) => {
+            const classCart = req.body;
+            console.log(classCart);
+            const result = await cartCollection.insertOne(classCart);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
